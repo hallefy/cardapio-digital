2ec8a78d40677385207a5dd575e8ca5a3ac33c10
hallefy <hallefy.ferreira@fretebras.com.br>
2021-07-01 22:39:00 -0300

    Added option to hide delivery

--- includes/home.php
+++ includes/home.php
@@ -953,7 +953,9 @@ endif;
 
 					$showOptions = $params['showOptions'];
 
-					if(!empty($confirm_delivery) && $confirm_delivery == "true" && $showOptions == true): ?>
+					if($showOptions == true)
+						return;
+					if(!empty($confirm_delivery) && $confirm_delivery == "true"): ?>
 						<div style="width: 100%;">
 							<div class="radio icheck-midnightblue">					
 								<input type="radio" required value='true' checked="" id="enterega" name="opcao_delivery" 
@@ -976,7 +978,10 @@ endif;
 
 					$showOptions = $params['showOptions'];
 
-					if(!empty($confirm_balcao) && $confirm_balcao == "true" && $showOptions == true): ?>
+					if($showOptions == true)
+						return;
+
+					if(!empty($confirm_balcao) && $confirm_balcao == "true"): ?>
 						<div style="width: 100%;">
 							<div class="radio icheck-midnightblue">					
 								<input type="radio" required value='false' id="buscar" name="opcao_delivery" 
