13f1c2dd052328ca0efe06e5c65b7224f9d3b3fd
hallefy <hallefy.ferreira@fretebras.com.br>
2021-07-05 13:38:39 -0300

    lp

--- landingpage.php
+++ landingpage.php
@@ -137,6 +137,7 @@ endif;
     <div>
      <img src="img/logo_branco.png" alt="Meu Kardapio" style="width:160px;height:160px;margin:15px">
     </div>
+    
         <font size='5'>Procurando <strong id="js-rotating">Restaurantes,Lanchonetes,Hamburguerias,Pizzarias,Sorveterias,Açaí</strong> ?</font>
         <br>
          <font size='3'> Os melhores cardápios estão aqui. Descubra! </font>
